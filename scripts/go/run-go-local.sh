7d5a53cfdc19b185936eac1a219c807d862d70e6
Emery Tanghanwaye <emerytang@gmail.com>
2024-11-29 10:27:16 -0500

    scripts: add local go run script

---
+++ scripts/go/run-go-local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname "$0")/../..
+PRIVATE_KEY=$(<"${BASE_DIR}/scripts/secrets/snitch_private_key.pem")
+
+export LIBSQL_HOST=localhost
+export LIBSQL_PORT=8081
+export LIBSQL_AUTH_KEY="$PRIVATE_KEY"
+
+go run "${BASE_DIR}"/cmd/snitchbe/main.go
