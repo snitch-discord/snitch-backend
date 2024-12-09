4470445be778fe5f96c115a773d72b25acd27093
Silvan <38138071+Sillyvan@users.noreply.github.com>
2024-12-10 00:16:32 +0100

    better local env

--- dev.sh
+++ dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-BASE_DIR=$(dirname "$0")/../..
+BASE_DIR=$(dirname "$0")
 PRIVATE_KEY=$(<"${BASE_DIR}/scripts/secrets/snitch_private_key.pem")
 
 export LIBSQL_HOST=localhost
@@ -8,4 +7,4 @@ export LIBSQL_PORT=8080
 export LIBSQL_ADMIN_PORT=9090
 export LIBSQL_AUTH_KEY="$PRIVATE_KEY"
 
-go run "${BASE_DIR}"/cmd/snitchbe/main.go
+go run ./cmd/snitchbe/main.go
