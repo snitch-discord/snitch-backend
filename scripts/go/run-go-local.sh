cb95b5657d0a3e6291d9ab8f54dd8b2b59241075
Emery Tanghanwaye <emerytang@gmail.com>
2024-12-09 17:40:29 -0500

    run-go-local.sh: fix env vars

--- scripts/go/run-go-local.sh
+++ scripts/go/run-go-local.sh
@@ -4,8 +4,8 @@ BASE_DIR=$(dirname "$0")/../..
 PRIVATE_KEY=$(<"${BASE_DIR}/scripts/secrets/snitch_private_key.pem")
 
 export LIBSQL_HOST=localhost
-export LIBSQL_PORT=8081
-export LIBSQL_ADMIN_PORT=9091
+export LIBSQL_PORT=8080
+export LIBSQL_ADMIN_PORT=9090
 export LIBSQL_AUTH_KEY="$PRIVATE_KEY"
 
 go run "${BASE_DIR}"/cmd/snitchbe/main.go
